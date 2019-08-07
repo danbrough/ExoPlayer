f2efa78643d0dac51b9ff49d18360ef4046f5c2c
Dan Brough <dan@danbrough.org>
2019-08-08 10:54:15 +1200

    upodate

--- extensions/flac/build.sh
+++ extensions/flac/build.sh
@@ -3,7 +3,10 @@
 cd `dirname $0`
 cd src/main/jni
 
-rm -rf flac
-curl https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.1.tar.xz | tar xJ && \
-mv flac-1.3.1 flac
+#rm -rf flac
+if [ ! -d flac ]; then 
+	curl https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.1.tar.xz | tar xJ && \
+	mv flac-1.3.1 flac
+fi
+
 ndk-build APP_ABI=all -j4

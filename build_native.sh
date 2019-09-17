28543f23bdcb1a4fa7ebe6177af56513d9ca0b3e
Dan Brough <dan@danbrough.org>
2019-09-17 18:18:18 +1200

    updated flac to 1.3.3

--- build_native.sh
+++ build_native.sh
@@ -8,8 +8,8 @@ NDK_PATH=/mnt/files/android/android-ndk-r17c
 
 cd "${FLAC_EXT_PATH}/jni" 
 if [ ! -d flac ]; then
-	curl https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.2.tar.xz | tar xJ && \
-	mv flac-1.3.2 flac
+	curl https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.3.tar.xz | tar xJ && \
+	mv flac-1.3.3 flac
 fi
 
 rm -rf "$FLAC_EXT_PATH/obj" "$FLAC_EXT_PATH/libs"

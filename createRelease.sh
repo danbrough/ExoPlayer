01d25080457d16c5cb25e0fa07ede4b120a7b547
Dan Brough <dan@danbrough.org>
2020-01-20 08:45:25 +1300

    update

--- createRelease.sh
+++ createRelease.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-https://jitpack.io/com/github/danbrough/exoplayer/2.11.1-dan04/build.log
+https://jitpack.io/com/github/danbrough/exoplayer/2.11.1-dan05/build.log
 
-
+https://jitpack.io/com/github/danbrough/ExoPlayer/2.11.1-dan02/build.log

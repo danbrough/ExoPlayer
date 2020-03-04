6c7f5ae38e883039423acf1af02f852c11f44410
Dan Brough <dan@danbrough.org>
2020-03-05 11:48:41 +1300

    r2.11.3-dan01

---
+++ build_native.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+EXOPLAYER_ROOT="$(pwd)"
+FLAC_EXT_PATH="${EXOPLAYER_ROOT}/extensions/flac/src/main"
+NDK_PATH=/mnt/files/android/android-ndk-r17c
+FLAC_VERSION=1.3.3
+
+cd "${FLAC_EXT_PATH}/jni" 
+if [ ! -d flac ]; then
+	git clone git@github.com:xiph/flac.git flac 
+	#curl https://ftp.osuosl.org/pub/xiph/releases/flac/flac-$FLAC_VERSION.tar.xz | tar xJ && \
+	#mv flac-$FLAC_VERSION flac
+fi
+cd flac && git clean -xdf && git reset --hard && git checkout $FLAC_VERSION && cd ..
+
+rm -rf "$FLAC_EXT_PATH/obj" "$FLAC_EXT_PATH/libs"
+cd "${FLAC_EXT_PATH}"/jni && \
+${NDK_PATH}/ndk-build APP_ABI=all -j4
+
+cd "$EXO_PLAYER_ROOT" 
+OPUS_EXT_PATH="${EXOPLAYER_ROOT}/extensions/opus/src/main"
+
+cd "${OPUS_EXT_PATH}/jni" 
+if [ ! -d libopus ]; then
+	git clone https://git.xiph.org/opus.git libopus
+else
+	cd libopus && git clean -xdf && git reset --hard && git pull 
+fi
+
+rm -rf "$OPUS_EXT_PATH/obj" "$OPUS_EXT_PATH/libs"
+cd "${OPUS_EXT_PATH}"/jni 
+./convert_android_asm.sh
+
+${NDK_PATH}/ndk-build APP_ABI=all -j4
+
+
+
+
+
+
+
---
+++ createRelease.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd `dirname $0`
+
+if [ ! -f .version ]; then
+	echo 0 > .version
+fi
+VERSION=`cat .version`
+VERSION=$(( $VERSION + 1 ))
+echo VERSION $VERSION
+echo $VERSION > .version
+
+VERSION_NAME=`printf 'r2.11.3-dan%02d'  $VERSION`
+echo VERSION_NAME $VERSION_NAME
+
+git commit -am "$VERSION_NAME"
+git tag "$VERSION_NAME"
+git push origin && git push origin "$VERSION_NAME"
+
+curl https://jitpack.io/com/github/danbrough/ExoPlayer/${VERSION_NAME}/build.log

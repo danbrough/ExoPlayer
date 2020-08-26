cb7f657e14f6ea5256d5f36e93fd9b328d0bc994
Dan Brough <dan@danbrough.org>
2020-08-26 12:41:04 +1200

    2.12.0-dan01

--- createRelease.sh
+++ createRelease.sh
@@ -11,9 +11,13 @@ echo $VERSION > .version
 
 VERSION_NAME=`printf '2.12.0-dan%02d'  $VERSION`
 echo VERSION_NAME $VERSION_NAME
-
+sed -i  constants.gradle -e 's:releaseVersion =.*:releaseVersion = "'$VERSION_NAME'":g'
 git commit -am "$VERSION_NAME"
+git tag -d "$VERSION_NAME"
 git tag "$VERSION_NAME"
-git push origin && git push origin "$VERSION_NAME"
+
+git push origin 
+git push origin --delete "$VERSION_NAME"
+git push origin "$VERSION_NAME"
 
 curl https://jitpack.io/com/github/danbrough/ExoPlayer/${VERSION_NAME}/build.log
